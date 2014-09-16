ddf2118f7aee06555595dff2e76911e89aeb6717
Rachel Denison <r.n.denison@gmail.com>
2014-09-15 20:07:17 -0400

    minor formatting edit for rd_getData.m

--- rd_getData.m
+++ rd_getData.m
@@ -1,4 +1,4 @@
-function [trigMean triggers Fs] =  rd_getData(filename, trigChan, channels, tstart, tstop)
+function [trigMean, triggers, Fs] =  rd_getData(filename, trigChan, channels, tstart, tstop)
 
 
 %% get data around each trigger
