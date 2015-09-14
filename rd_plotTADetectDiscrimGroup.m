3e9ceca464bd4d9b8306557012f93ad1680a08d3
Rachel Denison <r.n.denison@gmail.com>
2015-09-14 15:49:32 -0400

    removed the vals option in rd_plotTADetectDiscrimGroup.m. switched completely to groupData.

--- rd_plotTADetectDiscrimGroup.m
+++ rd_plotTADetectDiscrimGroup.m
@@ -5,7 +5,7 @@ exptDir = '/Volumes/DRIVE1/DATA/rachel/MEG/TADetectDiscrim/MEG';
 analStr = 'ebi_ft'; % '', 'ebi', etc.
 ssvefFreq = 30;
 nTopChannels = 5; % 1, 5, etc.
-measure = 'w'; % wAmpsAtt wAmpsPA hAmpsAtt hAmpsPA tf stf w
+measure = 'stf'; % w h tf stf
 
 subjects = {'R0817_20150504', 'R0973_20150727', 'R0974_20150728', ...
     'R0861_20150813', ...
@@ -60,183 +60,30 @@ for iSubject = 1:nSubjects
             groupData.ampsPA(:,:,:,iSubject) = A.stfAmpsPA;
             groupData.paDiff(:,:,:,iSubject) = A.stfPADiff;
         otherwise
-            v = A.(measure);
-            
-            if numel(size(v))==2
-                vals(:,:,iSubject) = v;
-            elseif numel(size(v))==3
-                vals(:,:,:,iSubject) = v;
-            else
-                error('too many or too few dimensions')
-            end
+            error('measure not recognized')
     end
 end
 
-if exist('vals','var')
-    dataType = 'vals';
-else
-    dataType = 'groupData';
+%% Calculate group mean and ste
+fieldNames = fieldnames(groupData);
+nFields = numel(fieldNames);
+for iF = 1:nFields
+    fieldName = fieldNames{iF};
+    vals = groupData.(fieldName);
+    sdim = numel(size(vals)); % subject dimension
+    groupMean.(fieldName) = mean(vals, sdim);
+    groupSte.(fieldName) = std(vals, 0, sdim)./sqrt(nSubjects);
 end
 
-%% Plots
-switch dataType
-    case 'vals'
-        %% Calculate
-        valsDiff = diff(vals);
-        
-        %% summary stats
-        sdim = numel(size(vals)); % subject dimension
-        
-        valsMean = squeeze(mean(vals,sdim));
-        valsSte = squeeze(std(vals,0,sdim)./sqrt(nSubjects));
-        
-        valsDiffMean = squeeze(mean(valsDiff,sdim));
-        valsDiffSte = squeeze(std(valsDiff,0,sdim)./sqrt(nSubjects));
-        
-        %% plot setup
-        nTrigs = numel(A.trigNames);
-        plotOrder = [1 5 3 7 2 6 4 8 9];
-        extendedMap = flipud(lbmap(nTrigs-1+4,'RedBlue'));
-        selectedMap = extendedMap([1:(nTrigs-1)/2 (end-(nTrigs-1)/2)+1:end],:);
-        trigColors = [selectedMap; 0 0 0];
-        trigBlue = mean(selectedMap(1:(nTrigs-1)/2,:));
-        trigRed = mean(selectedMap((end-(nTrigs-1)/2)+1:end,:));
-        trigColorsAtt2 = [trigBlue; trigRed];
-        trigColorsPA4 = [.52 .37 .75; .31 .74 .40; .27 .51 .84; 1.0 .57 .22];
-        
-        set(0,'defaultLineLineWidth',1)
-        
-        %% plot figs
-        condColors.Att = trigColorsAtt2;
-        condColors.PA = trigColorsPA4;
-        xlims = [t(1) t(end)];
-        condYlims.wAmps = [-.5 2.5];
-        condDiffLims.wAmps = [-.8 .8];
-        condYlims.hAmps = [-5 40];
-        condDiffLims.hAmps = [-5 5];
-        
-        switch measure
-            case 'wAmpsAtt'
-                m = 'wAmps';
-                p = 'Att';
-            case 'hAmpsAtt'
-                m = 'hAmps';
-                p = 'Att';
-            case 'wAmpsPA'
-                m = 'wAmps';
-                p = 'PA';
-            case 'hAmpsPA'
-                m = 'hAmps';
-                p = 'PA';
-            otherwise
-                error('measure not recognized')
-        end
-        ylims = condYlims.(m);
-        diffLims = condDiffLims.(m);
-        colors = condColors.(p);
-        
-        %% indiv subjects
-        figure
-        set(gcf,'Position',[30 450 1200 250])
-        for iSubject = 1:nSubjects
-            subplot(nSubjects,1,iSubject)
-            hold on
-            for i=1:size(valsMean,1)
-                plot(t, vals(i,:,iSubject), 'color', colors(i,:))
-            end
-            plot(xlims, [0 0], 'k')
-            xlim(xlims)
-            ylim(ylims)
-            for iEv = 1:numel(evTimes)
-                vline(evTimes(iEv),'color','k','LineStyle',':');
-            end
-            if iSubject==1
-                xlabel('time (ms)')
-                ylabel('amplitude')
-            end
-        end
-        
-        if strcmp(p,'Att')
-            figure
-            set(gcf,'Position',[30 450 1200 250])
-            for iSubject = 1:nSubjects
-                subplot(1,nSubjects,iSubject)
-                hold on
-                plot(t, valsDiff(1,:,iSubject), 'k', 'LineWidth', 2)
-                plot(xlims, [0 0], 'k')
-                xlim(xlims)
-                ylim(diffLims)
-                for iEv = 1:numel(evTimes)
-                    vline(evTimes(iEv),'color','k','LineStyle',':');
-                end
-                if iSubject==1
-                    xlabel('time (ms)')
-                    ylabel('amplitude difference (T2-T1)')
-                end
-            end
-        end
-        
-        %% group
-        figure
-        hold on
-        for i=1:size(valsMean,1)
-            shadedErrorBar(t, valsMean(i,:), valsSte(i,:), {'color', colors(i,:), 'LineWidth', 3}, 1)
-        end
-        plot(xlims, [0 0], 'k')
-        for iEv = 1:numel(evTimes)
-            vline(evTimes(iEv),'color','k','LineStyle',':');
-        end
-        xlabel('time (ms)')
-        ylabel('amplitude')
-        
-        if strcmp(p,'Att')
-            figure
-            hold on
-            for i=1:2
-                shadedErrorBar(t, valsMean(i,:), valsDiffSte, {'color', colors(i,:), 'LineWidth', 3}, 1)
-            end
-            plot(xlims, [0 0], 'k')
-            for iEv = 1:numel(evTimes)
-                vline(evTimes(iEv),'color','k','LineStyle',':');
-            end
-            xlabel('time (ms)')
-            ylabel('amplitude')
-            
-            figure
-            hold on
-            shadedErrorBar(t, valsDiffMean, valsDiffSte, 'k', 1)
-            plot(xlims, [0 0], 'k')
-            ylim(diffLims)
-            for iEv = 1:numel(evTimes)
-                vline(evTimes(iEv),'color','k','LineStyle',':');
-            end
-            xlabel('time (ms)')
-            ylabel('amplitude difference (T2-T1)')
-        end
-        
-    case 'groupData'
-        %% calculate group mean and ste
-        fieldNames = fieldnames(groupData);
-        nFields = numel(fieldNames);
-        for iF = 1:nFields
-            fieldName = fieldNames{iF};
-            vals = groupData.(fieldName);
-            sdim = numel(size(vals)); % subject dimension
-            groupMean.(fieldName) = mean(vals, sdim);
-            groupSte.(fieldName) = std(vals, 0, sdim)./sqrt(nSubjects);
-        end
-        
-        %% plot figs
-        switch measure
-            case {'w','h'}
-                rd_plotTADetectDiscrimGroupTS(A, measure, ...
-                    groupData, groupMean, groupSte)
-            case {'tf','stf'}
-                rd_plotTADetectDiscrimGroupTimeFreq(A, measure, ...
-                    groupMean.amps, groupMean.ampsAtt, groupMean.ampsPA, groupMean.paDiff)
-            otherwise
-                error('measure not recognized')
-        end
+%% Plot figs
+switch measure
+    case {'w','h'}
+        rd_plotTADetectDiscrimGroupTS(A, measure, ...
+            groupData, groupMean, groupSte)
+    case {'tf','stf'}
+        rd_plotTADetectDiscrimGroupTimeFreq(A, measure, ...
+            groupMean.amps, groupMean.ampsAtt, groupMean.ampsPA, groupMean.paDiff)
     otherwise
-        error('dataType not recognized')
+        error('measure not recognized')
 end
+
